2269a082ce4b6daa5b442fb5a6392cb19705e0e0
Aaron Lin <aaron.tlin@gmail.com>
2020-08-24 20:08:34 -0400

    working version of multiple tabs at once before testing multithreading

--- bot.py
+++ bot.py
@@ -8,6 +8,15 @@ import os
 from contextlib import suppress
 import regex as re
 from config import key, current_machine_id
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from multiprocessing import Process
+
+#Windows Version 1.0.0
+
+
+
+
 
 if current_machine_id == key:
 #with suppress(Exception):
@@ -28,24 +37,36 @@ if current_machine_id == key:
     options = webdriver.ChromeOptions()
     options.add_argument('ignore-certificate-errors-spki-list')
     options.add_argument('ignore-ssl-errors')
-    options.add_argument('headless')
+    #options.add_argument('headless')
     driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
     results = []
     #parses through every url
-    for i,url in enumerate(URLS):
-        print(url)
+    print(len(URLS))
+    for i, urls in enumerate(URLS):
+        js_string = "window.open('" + urls + "'); "
+        driver.execute_script(js_string)
+        print("new tab")
+        # driver.switch_to.window(driver.window_handles[i])
+        sleep(1)
+        # driver.execute_script("window.open('your url','_blank');")
+        # driver.get(urls)
+    for i, urls in enumerate(URLS):
+        driver.switch_to.window(driver.window_handles[i])    
+        print(i)
         terms=[]
         definitions = []
         result = []
         correct_flashcard = []
         print("web start load")
-        driver.get(url)
         print("url gotten")
         src = driver.page_source
         soup = BeautifulSoup(src, "html.parser")
-        location = None
         flashcards = soup.findAll('span', {"class": "TermText notranslate lang-en" })
         print("term start")
+
+#can multi thread this for loop to be faster
+        
+    # for i in enumerate(URLS):
         for term, definition in zip(flashcards[::2], flashcards[1::2]):
             if fuzz.ratio(term.text, keyphrase) > 80:
                 correct_flashcard.append(term.text)
@@ -60,15 +81,8 @@ if current_machine_id == key:
         #result.append(terms)
         #result.append(definitions)
         results.append(result)
-        # for i, a in (0, enumerate():
-        #     if fuzz.ratio(a.text, keyphrase) > 60:
-        #         terms.append(a.text)
-        #         location = i
-        #     #add so that the definition can be flipped                
-        #     if location is not None:
-        #         if i == location + 1:
-        #             definitions.append(a.text)
-        #result.append(terms)
+
+
 
 
     driver.close()
@@ -77,3 +91,6 @@ if current_machine_id == key:
         if result != [[]]:
             print(result)
             print()
+
+# https://testdriven.io/blog/building-a-concurrent-web-scraper-with-python-and-selenium/
+# https://stackoverflow.com/questions/47543795/what-is-the-fastest-way-to-open-urls-in-new-tabs-via-selenium-python
