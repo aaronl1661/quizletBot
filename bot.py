9a05c8936d5cf5eb1c72f0cef1b557a0da3ed501
Aaron Lin <aaron.tlin@gmail.com>
2020-08-25 23:09:12 -0400

    commit with search threading complete before i test threading multiple questions

--- bot.py
+++ bot.py
@@ -11,7 +11,9 @@ from config import key, current_machine_id
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 from _thread import start_new_thread
-
+from threading import Thread
+from multiprocessing.pool import ThreadPool
+import datetime
 
 #Windows Version 1.0.0
 
@@ -20,9 +22,32 @@ def open_tab(url, driver):
     js_string = "window.open('" + urls + "'); "
     driver.execute_script(js_string)
     return True
-
-def parse_tab(driver):
-    return True
+results = []
+def parse_tab(i ,driver):
+    driver.switch_to.window(driver.window_handles[i])
+    print('getting source')
+    print(driver.window_handles[i])
+    src = driver.page_source
+    print('src recieved and now parsing')
+    soup = BeautifulSoup(src, "lxml")
+    print('parsing finished')
+    flashcards = soup.findAll('span', {"class": "TermText notranslate lang-en" })
+    print('found flashcards')
+    result = []
+    correct_flashcard = []
+    for term, definition in zip(flashcards[::2], flashcards[1::2]):
+        if fuzz.ratio(term.text, keyphrase) > 80:
+            correct_flashcard.append(term.text)
+            correct_flashcard.append(definition.text)
+        elif fuzz.ratio(definition.text, keyphrase) > 80:
+            correct_flashcard.append(definition.text)
+            correct_flashcard.append(term.text)
+        else:
+            continue
+    result.append(correct_flashcard)
+    results.append(result)
+    print(result)
+    return result
 
 if current_machine_id == key:
 #with suppress(Exception):
@@ -43,7 +68,8 @@ if current_machine_id == key:
     options = webdriver.ChromeOptions()
     options.add_argument('ignore-certificate-errors-spki-list')
     options.add_argument('ignore-ssl-errors')
-    #options.add_argument('headless')
+    # options.add_argument('--headless')
+    #options.add_argument('--disable_gpu')
     driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
     results = []
     #parses through every url
@@ -59,47 +85,27 @@ if current_machine_id == key:
         # sleep(1)
         # driver.execute_script("window.open('your url','_blank');")
         # driver.get(urls)
-    for i, urls in enumerate(URLS):
-        driver.switch_to.window(driver.window_handles[i])
-        print(i)
-        terms=[]
-        definitions = []
-        result = []
-        correct_flashcard = []
-        print("web start load")
-        print("url gotten")
-        src = driver.page_source
-        soup = BeautifulSoup(src, "html.parser")
-        flashcards = soup.findAll('span', {"class": "TermText notranslate lang-en" })
-        print("term start")
+    threads = []
+    for i in range(0, len(URLS)):
+        print(datetime.time())
+        process = Thread(target=parse_tab, args=[i ,driver])
+        process.start()
+        threads.append(process)
 
-#can multi thread this for loop to be faster
-        
-    # for i in enumerate(URLS):
-        for term, definition in zip(flashcards[::2], flashcards[1::2]):
-            if fuzz.ratio(term.text, keyphrase) > 80:
-                correct_flashcard.append(term.text)
-                correct_flashcard.append(definition.text)
-            elif fuzz.ratio(definition.text, keyphrase) > 80:
-                correct_flashcard.append(definition.text)
-                correct_flashcard.append(term.text)
-            else:
-                continue
-        print("term end")
-        result.append(correct_flashcard)
-        #result.append(terms)
-        #result.append(definitions)
-        results.append(result)
-
-
-
-
+        # start_new_thread(parse_tab, (driver,))
+    for process in threads:
+        process.join()
     driver.close()
+    
     os.system('cls')
-    for result in results:
-        if result != [[]]:
-            print(result)
-            print()
+    print('done')
+    print(results)
+    exit()
+    # os.system('cls')
+    # for result in results:
+    #     if result != [[]]:
+    #         print(result)
+    #         print()
 
 # https://testdriven.io/blog/building-a-concurrent-web-scraper-with-python-and-selenium/
 # https://stackoverflow.com/questions/47543795/what-is-the-fastest-way-to-open-urls-in-new-tabs-via-selenium-python
